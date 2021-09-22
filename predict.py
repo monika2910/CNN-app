a695d337b3e055eab126c0d6edc123d979352b62
monika2910 <32133030+monika2910@users.noreply.github.com>
2021-09-22 11:16:01 +0100

    files added

---
+++ predict.py
@@ -0,0 +1,47 @@
+"""
+author: monika singh
+date: 10/09/2021
+"""
+
+import os
+import utils.config as config
+import tensorflow as tf
+import tensorflow.keras as tf_keras
+
+from tensorflow import keras
+
+
+import utils.data_management as dm
+import matplotlib.pyplot as plt
+
+
+class Predict:
+    def __init__(self, latest=True, model_index=None):
+        if latest:
+            self.get_latest_model_path()
+        elif (model_index is not None) and (not latest):
+            self.model_index = model_index
+            self.get_other_models() 
+        self.my_model = tf_keras.models.load_model(self.latest_model_path)
+
+    def get_latest_model_path(self):
+        available_models = os.listdir(config.TRAINED_MODEL_DIR)
+        latest_model = sorted(available_models)[-1]
+        self.latest_model_path = os.path.join(config.TRAINED_MODEL_DIR, latest_model)
+
+    def get_other_models(self):
+        available_models = os.listdir(config.TRAINED_MODEL_DIR)
+        latest_model = sorted(available_models)[self.model_index]
+        self.latest_model_path = os.path.join(config.TRAINED_MODEL_DIR, latest_model)
+
+    def predict(self, input_img_path=None):
+        img = plt.imread(input_img_path)
+        fit_img = dm.manage_input_data(img)
+        result = self.my_model.predict(fit_img)
+        print("### RESULT: ", result)
+
+if __name__ == "__main__":
+    obj = Predict()
+    obj.predict(input_img_path='dog.jpg')
+
+    
---
+++ utils/model.py
@@ -0,0 +1,88 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+import keras.applications.vgg16
+import numpy as np
+
+
+import utils.config as config
+import time
+
+def save_vgg_16_model(input_shape=config.IMAGE_SIZE):
+    model = keras.applications.vgg16.VGG16(
+        input_shape=input_shape,
+        weights="imagenet",
+        include_top=False
+    )
+    model.save("original_vgg_base.h5")
+    print("base model is saved")
+
+def load_base_model():
+    model = keras.models.load_model("utils/original_vgg_base.h5")
+    print("original base model is loaded")
+    model.summary()
+    return model
+
+def custom_model(CLASSES=config.CLASSES, freeze_all=True, freeze_till=None):
+    model = load_base_model()
+
+    # freeze weights - 
+    if freeze_all:
+        for layer in model.layers:
+            layer.trainable = False
+    elif (freeze_till is not None) and (freeze_till > 0):
+        for layer in model.layers[:freeze_till]:
+            layer.trainable = False
+
+    # add custom layers -
+    flatten_in = tf.keras.layers.Flatten()(model.output)
+    prediction = tf.keras.layers.Dense(
+        units=CLASSES,
+        activation="softmax"
+    )(flatten_in)
+
+    full_model = tf.keras.models.Model(
+        inputs=model.input,
+        outputs = prediction
+    )
+    print("custom model summary")
+    full_model.summary()
+
+    full_model.compile(
+        optimizer = tf.keras.optimizers.SGD(lr=0.005, momentum=0.9),
+        loss = tf.keras.losses.CategoricalCrossentropy(),
+        metrics = ["accuracy"]
+    )
+
+    return full_model
+
+
+def callbacks(base_dir="."):
+
+    # tensorboard callbacks
+    base_log_dir = config.TENSORBOARD_ROOT_LOG_DIR
+    unique_log = time.strftime("log_at_%Y%m%d_%H%M%S")
+    tensorboard_log_dir = os.path.join(base_log_dir, unique_log)
+    os.makedirs(tensorboard_log_dir, exist_ok=True)
+
+    tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_log_dir)
+
+    # checkpoint callbacks
+
+    checkpoint_file = os.path.join(config.CHECKPOINT_DIR, "vgg_16model_checkpoint.h5")
+    os.makedirs(config.CHECKPOINT_DIR, exist_ok=True)
+
+    checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_file,
+        save_best_only=True
+    )
+
+    callback_list = [tensorboard_cb, checkpoint_cb]
+
+    return callback_list
+
+
+if __name__ == "__main__":
+    #save_vgg_16_model()
+    #load_base_model()
+    custom_model()
